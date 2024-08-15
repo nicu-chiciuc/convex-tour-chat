fb49b9888bcde99e8f011c4e78504ba2d5ed541c
Nicu Chiciuc <nicuchiciuc@gmail.com>
2024-08-15 17:44:40 +0300

    update schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,7 +1,11 @@
 import { defineSchema, defineTable } from "convex/server";
+import { authTables } from "@convex-dev/auth/server";
+
 import { v } from "convex/values";
 
 export default defineSchema({
+  ...authTables,
+
   messages: defineTable({
     author: v.string(),
     body: v.string(),

58d8d381736383204a8f8a92b890214485aeb26c
Nicu Chiciuc <nicuchiciuc@gmail.com>
2024-08-15 17:43:16 +0300

    npx @convex-dev/auth

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,8 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as ai from "../ai.js";
+import type * as auth from "../auth.js";
+import type * as http from "../http.js";
 import type * as init from "../init.js";
 import type * as messages from "../messages.js";
 
@@ -28,6 +30,8 @@ import type * as messages from "../messages.js";
  */
 declare const fullApi: ApiFromModules<{
   ai: typeof ai;
+  auth: typeof auth;
+  http: typeof http;
   init: typeof init;
   messages: typeof messages;
 }>;
